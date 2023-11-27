e9d288499c0057427526b663683943fe82e1e5d9
Amasil Rahim Zihad <amasilrahim.zihad@ucalgary.ca>
2023-11-27 11:21:44 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function login(provider) {
+    alert(`You clicked on "Login with ${provider}" button.`);
+    // You can add your login logic for each provider here
+    // For a simple example, we're just displaying an alert
+  }
+  
