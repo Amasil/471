0ea93b673d311c5ba67ecc5ccbdb90742b339232
Amasil Rahim Zihad <amasilrahim.zihad@ucalgary.ca>
2023-12-01 19:58:54 -0700

    Connection Improved

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,38 @@
 // run this code with `node server.js`
 
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql2");
 
 const app = express();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Shemiran1234@',
-  database: 'sample_db'
+  host: "localhost",
+  user: "root",
+  password: "sZ10O84<",
+  database: "Test",
+  authPlugins: {
+    mysql_clear_password: () => () => Buffer.from("sZ10O84<"),
+  },
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to database: ' + err.stack);
+    console.error("Error connecting to database: " + err.stack);
     return;
   }
 
-  console.log('Connected to database.');
+  console.log("Connected to database.");
 });
 
 // this is the api route
 // to make a GET u do app.get
 // to do a POST u do app.post
 // use postman to verify integrity of the api's
-app.get('/users', (req, res) => {
-  connection.query('SELECT * FROM users', (err, results) => {
+app.get("/users", (req, res) => {
+  connection.query("SELECT * FROM users", (err, results) => {
     if (err) {
-      console.error('Error querying database: ' + err.stack);
-      res.status(500).send('Error querying database.');
+      console.error("Error querying database: " + err.stack);
+      res.status(500).send("Error querying database.");
       return;
     }
 
@@ -38,5 +41,5 @@ app.get('/users', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000.');
+  console.log("Server listening on port 3000.");
 });
