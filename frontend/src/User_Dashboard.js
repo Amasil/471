a5dd4639868c5718b23199967760217a0f2a297a
Amasil Rahim Zihad <amasilrahim.zihad@ucalgary.ca>
2023-12-02 11:48:15 -0700

    User Dashboard Updated

--- frontend/src/User_Dashboard.js
+++ frontend/src/User_Dashboard.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import "./Users.css"; // Import the CSS file
+
 const Users = () => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
@@ -19,19 +21,19 @@ const Users = () => {
   }, []);
 
   const handleUserSelect = (userId) => {
-    // Find the selected user based on the User_ID
     const selected = users.find((user) => user.User_ID === userId);
     setSelectedUser(selected);
   };
 
   return (
-    <div>
+    <div className="users-container">
       <h1>Test All Users</h1>
-      <div>
-        {/* Dropdown to select users */}
-        <select onChange={(e) => handleUserSelect(parseInt(e.target.value))}>
+      <div className="user-dropdown-container">
+        <select
+          className="user-dropdown"
+          onChange={(e) => handleUserSelect(parseInt(e.target.value))}
+        >
           <option value={null}>Select a user</option>
-          {/* Map through the users and create an option for each */}
           {users.map((user) => (
             <option key={user.User_ID} value={user.User_ID}>
               {user.First_Name} {user.Middle_Name || ""} {user.Last_Name}
@@ -39,15 +41,13 @@ const Users = () => {
           ))}
         </select>
 
-        {/* Display selected user information */}
         {selectedUser && (
-          <div className="user-details">
+          <div className="user-details-container">
             <h2>User Information</h2>
             <p>User ID: {selectedUser.User_ID}</p>
             <p>First Name: {selectedUser.First_Name}</p>
             <p>Middle Name: {selectedUser.Middle_Name || "N/A"}</p>
             <p>Last Name: {selectedUser.Last_Name}</p>
-            {/* Add other user information as needed */}
           </div>
         )}
       </div>
