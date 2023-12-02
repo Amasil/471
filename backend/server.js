4061cf6ee85a1bcc453a57d8b88407f8e4c5ecc0
Amasil Rahim Zihad <amasilrahim.zihad@ucalgary.ca>
2023-12-01 20:09:19 -0700

    server.js commented

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
-// run this code with `node server.js`
-
+// Import required modules
 const express = require("express");
 const mysql = require("mysql2");
 
+// Create an Express application
 const app = express();
 
+// Set up a MySQL database connection configuration
 const connection = mysql.createConnection({
+  // Database host
   host: "localhost",
+  // Database user
   user: "root",
+  // Database password
   password: "sZ10O84<",
+  // Database port
   database: "Test",
+  // Authentication method
   authPlugins: {
     mysql_clear_password: () => () => Buffer.from("sZ10O84<"),
   },
 });
 
+// Connect to the MySQL database
 connection.connect((err) => {
   if (err) {
     console.error("Error connecting to database: " + err.stack);
@@ -24,22 +31,23 @@ connection.connect((err) => {
   console.log("Connected to database.");
 });
 
-// this is the api route
-// to make a GET u do app.get
-// to do a POST u do app.post
-// use postman to verify integrity of the api's
+// Define an API route for handling GET requests to "/users"
 app.get("/users", (req, res) => {
+  // Execute a SELECT query on the "users" table
   connection.query("SELECT * FROM users", (err, results) => {
     if (err) {
+      // Handle database query error
       console.error("Error querying database: " + err.stack);
       res.status(500).send("Error querying database.");
       return;
     }
 
+    // Send the query results as the response
     res.send(results);
   });
 });
 
+// Start the Express server on port 3000
 app.listen(3000, () => {
   console.log("Server listening on port 3000.");
 });
