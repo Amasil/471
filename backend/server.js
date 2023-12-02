5da8bd74d33f21b7f87b92a92e7ac44ef7007b10
Amasil Rahim Zihad <amasilrahim.zihad@ucalgary.ca>
2023-12-02 00:11:58 -0700

    Create User Now Working.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -50,7 +52,7 @@ app.post("/user", (req, res) => {
     Blood_Group,
     Last_Donation_Date,
   } = req.body;
-
+  console.log(req.body);
   if (
     !First_Name ||
     !Last_Name ||
