eb70d494ee4a1ed72ad2cf79fa69a12c0c5f2546
zainab1712 <zainabwaseem1703@gmail.com>
2023-12-10 15:47:36 -0700

    Alerts and updates for inventory

--- frontend/src/Inventory.js
+++ frontend/src/Inventory.js
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
-import "./dashboard.css"; // Assuming you have a CSS file for styling
+import "./dashboard.css";
+import AdminSidebar from "./AdminSidebar.js";
 
 const Inventory = () => {
-  const [sendAlerts, setSendAlerts] = useState({
-    "A+": false,
-    "A-": false,
-    "B+": false,
-    "B-": false,
-    "AB+": false,
-    "AB-": false,
-    "O+": false,
-    "O-": false,
-  });
-
   const [editableQuantities, setEditableQuantities] = useState({
     "A+": false,
     "A-": false,
@@ -35,15 +25,34 @@ const Inventory = () => {
     "O-": 20,
   });
 
-  const handleSendAlert = (bloodType, status) => {
-    if (status === "Low") {
-      setSendAlerts((prevSendAlerts) => ({
-        ...prevSendAlerts,
-        [bloodType]: true,
-      }));
+  const [status, setStatus] = useState({
+    "A+": "Healthy",
+    "A-": "Healthy",
+    "B+": "Healthy",
+    "B-": "Healthy",
+    "AB+": "Healthy",
+    "AB-": "Healthy",
+    "O+": "Healthy",
+    "O-": "Healthy",
+  });
 
-      alert("Alert sent!");
-    }
+  const [sendAlert, setSendAlert] = useState({
+    "A+": false,
+    "A-": false,
+    "B+": false,
+    "B-": false,
+    "AB+": false,
+    "AB-": false,
+    "O+": false,
+    "O-": false,
+  });
+
+  const handleSendAlert = (bloodType) => {
+    setSendAlert((prevSendAlert) => ({
+      ...prevSendAlert,
+      [bloodType]: true,
+    }));
+    alert("Alert sent!");
   };
 
   const handleUpdateQuantity = (bloodType) => {
@@ -54,100 +63,93 @@ const Inventory = () => {
   };
 
   const handleSaveQuantity = (bloodType) => {
+    const updatedQuantities = {
+      ...quantities,
+      [bloodType]: parseInt(quantities[bloodType], 10),
+    };
+
     setEditableQuantities((prevEditableQuantities) => ({
       ...prevEditableQuantities,
       [bloodType]: false,
     }));
 
-    // Save logic - Update the state with the new quantity
-    setQuantities((prevQuantities) => ({
-      ...prevQuantities,
-      [bloodType]: parseInt(prevQuantities[bloodType], 10), // Convert to integer if needed
-    }));
+    setQuantities(updatedQuantities);
 
-    // If you have a server or database, you can send the updated quantity data here
-    // For example, you can use fetch or axios to make an API call to update the backend
-    // fetch('your-api-endpoint', {
-    //   method: 'PUT',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     bloodType,
-    //     quantity: parseInt(prevQuantities[bloodType], 10),
-    //   }),
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data))
-    //   .catch((error) => console.error('Error:', error));
+    if (parseInt(updatedQuantities[bloodType], 10) < 30) {
+      setStatus((prevStatus) => ({
+        ...prevStatus,
+        [bloodType]: "Low",
+      }));
+      alert(`Status for ${bloodType} set to Low due to low quantity.`);
+    }
   };
 
   const handleQuantityChange = (bloodType, event) => {
-    const updatedQuantities = { ...quantities, [bloodType]: parseInt(event.target.value, 10) };
+    const updatedQuantities = {
+      ...quantities,
+      [bloodType]: parseInt(event.target.value, 10),
+    };
     setQuantities(updatedQuantities);
   };
 
   return (
-    <div>
-      <header>
-        <h1>Inventory Dashboard</h1>
-      </header>
+    <div className="admin-dashboard">
+      <AdminSidebar />
+      <div className="inventory-container">
+        <header>
+          <h1>Inventory Dashboard</h1>
+        </header>
 
-      <nav>
-        <a href="doctorDashboard.html">Home</a>
-        <a href="inventory.html">Inventory</a>
-        <a href="#">Logout</a>
-      </nav>
+        <section>
+          <h2>Inventory</h2>
 
-      <section>
-        <h2>Inventory</h2>
-
-        <table>
-          <thead>
-            <tr>
-              <th>Blood Type</th>
-              <th>Quantity (units)</th>
-              <th>Status</th>
-              <th>Update</th>
-              <th>Send Alert</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(quantities).map((bloodType) => (
-              <tr key={bloodType}>
-                <td>{bloodType}</td>
-                <td>
-                  {editableQuantities[bloodType] ? (
-                    <input
-                      type="text"
-                      value={quantities[bloodType]}
-                      onChange={(e) => handleQuantityChange(bloodType, e)}
-                    />
-                  ) : (
-                    quantities[bloodType]
-                  )}
-                </td>
-                <td>Healthy</td>
-                <td>
-                  {editableQuantities[bloodType] ? (
-                    <button onClick={() => handleSaveQuantity(bloodType)}>Save</button>
-                  ) : (
-                    <button onClick={() => handleUpdateQuantity(bloodType)}>Update</button>
-                  )}
-                </td>
-                <td>
-                  <button
-                    disabled={sendAlerts[bloodType]}
-                    onClick={() => handleSendAlert(bloodType, "Low")}
-                  >
-                    Send Alert
-                  </button>
-                </td>
+          <table>
+            <thead>
+              <tr>
+                <th>Blood Type</th>
+                <th>Quantity (units)</th>
+                <th>Status</th>
+                <th>Update</th>
+                <th>Send Alert</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
+            </thead>
+            <tbody>
+              {Object.keys(quantities).map((bloodType) => (
+                <tr key={bloodType}>
+                  <td>{bloodType}</td>
+                  <td>
+                    {editableQuantities[bloodType] ? (
+                      <input
+                        type="text"
+                        value={quantities[bloodType]}
+                        onChange={(e) => handleQuantityChange(bloodType, e)}
+                      />
+                    ) : (
+                      quantities[bloodType]
+                    )}
+                  </td>
+                  <td>{status[bloodType]}</td>
+                  <td>
+                    {editableQuantities[bloodType] ? (
+                      <button onClick={() => handleSaveQuantity(bloodType)}>Save</button>
+                    ) : (
+                      <button onClick={() => handleUpdateQuantity(bloodType)}>Update</button>
+                    )}
+                  </td>
+                  <td>
+                    <button
+                      disabled={status[bloodType] !== "Low" || sendAlert[bloodType]}
+                      onClick={() => handleSendAlert(bloodType)}
+                    >
+                      Send Alert
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
+      </div>
     </div>
   );
 };
