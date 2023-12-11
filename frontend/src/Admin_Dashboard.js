c6454d10de30453de2d7b8243c6b3f9afb1411c5
Amasil Rahim Zihad <amasilrahim.zihad@ucalgary.ca>
2023-12-10 20:58:34 -0700

    Delete Button hidden

--- frontend/src/Admin_Dashboard.js
+++ frontend/src/Admin_Dashboard.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route, Link, useLocation } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import axios from "axios";
 import AdminSidebar from "./AdminSidebar.js";
 import "./Admin_Dashboard.css";
-import Navbar from "./App.js";
 
 const updateFavicon = (faviconURL) => {
   const favicon = document.querySelector('link[rel="icon"]');
@@ -58,6 +56,8 @@ const Users = () => {
     Last_Donation_Date: "",
   });
   const [isEditing, setIsEditing] = useState(false);
+  const [isDeleteButtonDisabled, setIsDeleteButtonDisabled] = useState(true);
+  const [cancelClicked, setCancelClicked] = useState(false);
 
   useEffect(() => {
     const fetchAllUsers = async () => {
@@ -76,7 +76,6 @@ const Users = () => {
     const selected = users.find((user) => user.User_ID === userId);
     setSelectedUser(selected);
 
-    // Populate all the empty fields with the user's information
     setNewUserInfo({
       User_ID: selected.User_ID || "",
       First_Name: selected.First_Name || "",
@@ -90,7 +89,9 @@ const Users = () => {
       Last_Donation_Date: selected.Last_Donation_Date || "",
     });
 
-    setIsEditing(false); // Reset editing mode
+    setIsDeleteButtonDisabled(false);
+    setIsEditing((prevIsEditing) => !prevIsEditing);
+    setCancelClicked(false); // Reset cancelClicked state
   };
 
   const handleChangeUserInfo = async () => {
@@ -116,6 +117,17 @@ const Users = () => {
     }
 
     setIsEditing(false);
+    setIsDeleteButtonDisabled(true); // Disable delete button when changes are saved
+    setCancelClicked(false); // Reset cancelClicked state
+  };
+
+  const handleDeleteUserAndCancel = async (userId) => {
+    try {
+      await handleDeleteUser(userId);
+      setCancelClicked(true);
+    } catch (error) {
+      console.error("Error deleting user:", error);
+    }
   };
 
   const handleDeleteUser = async () => {
@@ -144,6 +156,7 @@ const Users = () => {
           Last_Donation_Date: "",
         });
         setIsEditing(false);
+        setIsDeleteButtonDisabled(true); // Disable delete button after user is deleted
       } else {
         console.log("No user selected to delete.");
       }
@@ -155,184 +168,205 @@ const Users = () => {
   return (
     <div className="admin-dashboard">
       <div className="users-container">
-        <h1>Select a user to edit</h1>
-        <div className="user-dropdown-container">
-          <select
-            className="user-dropdown"
-            onChange={(e) => handleUserSelect(parseInt(e.target.value))}
-          >
-            <option value={null}>Select a user</option>
+        <h1>User List</h1>
+        <table className="user-table">
+          <thead>
+            <tr>
+              <th>User ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
             {users.map((user) => (
-              <option key={user.User_ID} value={user.User_ID}>
-                {user.First_Name} {user.Middle_Name || ""} {user.Last_Name}
-              </option>
+              <React.Fragment key={user.User_ID}>
+                <tr>
+                  <td>{user.User_ID}</td>
+                  <td>{user.First_Name}</td>
+                  <td>{user.Last_Name}</td>
+                  <td>
+                    <button onClick={() => handleUserSelect(user.User_ID)}>
+                      {isEditing &&
+                      selectedUser &&
+                      selectedUser.User_ID === user.User_ID
+                        ? "Cancel"
+                        : "Edit"}
+                    </button>
+                    {isEditing &&
+                      selectedUser &&
+                      selectedUser.User_ID === user.User_ID && (
+                        <button
+                          onClick={() =>
+                            handleDeleteUserAndCancel(user.User_ID)
+                          }
+                          disabled={isDeleteButtonDisabled || cancelClicked}
+                          className="delete-button"
+                        >
+                          Delete
+                        </button>
+                      )}
+                  </td>
+                </tr>
+                {isEditing &&
+                  selectedUser &&
+                  selectedUser.User_ID === user.User_ID && (
+                    <tr>
+                      <td colSpan="4">
+                        <div>
+                          <h2>Edit User Information</h2>
+                          <label>
+                            First Name:
+                            <input
+                              type="text"
+                              value={newUserInfo.First_Name}
+                              onChange={(e) =>
+                                setNewUserInfo({
+                                  ...newUserInfo,
+                                  First_Name: e.target.value,
+                                })
+                              }
+                            />
+                          </label>
+                          <label>
+                            Middle Name:
+                            <input
+                              type="text"
+                              value={newUserInfo.Middle_Name}
+                              onChange={(e) =>
+                                setNewUserInfo({
+                                  ...newUserInfo,
+                                  Middle_Name: e.target.value,
+                                })
+                              }
+                            />
+                          </label>
+                          <br />
+                          <label>
+                            Last Name:
+                            <input
+                              type="text"
+                              value={newUserInfo.Last_Name}
+                              onChange={(e) =>
+                                setNewUserInfo({
+                                  ...newUserInfo,
+                                  Last_Name: e.target.value,
+                                })
+                              }
+                            />
+                          </label>
+                          <br />
+                          <label>
+                            Username:
+                            <input
+                              type="text"
+                              value={newUserInfo.Username}
+                              onChange={(e) =>
+                                setNewUserInfo({
+                                  ...newUserInfo,
+                                  Username: e.target.value,
+                                })
+                              }
+                            />
+                          </label>
+                          <br />
+                          <label>
+                            Password:
+                            <input
+                              type="text"
+                              value={newUserInfo.Password}
+                              onChange={(e) =>
+                                setNewUserInfo({
+                                  ...newUserInfo,
+                                  Password: e.target.value,
+                                })
+                              }
+                            />
+                          </label>
+                          <br />
+                          <label>
+                            Email:
+                            <input
+                              type="text"
+                              value={newUserInfo.Email}
+                              onChange={(e) =>
+                                setNewUserInfo({
+                                  ...newUserInfo,
+                                  Email: e.target.value,
+                                })
+                              }
+                            />
+                          </label>
+                          <br />
+                          <label>
+                            Phone No:
+                            <input
+                              type="text"
+                              value={newUserInfo.Phone_No}
+                              onChange={(e) =>
+                                setNewUserInfo({
+                                  ...newUserInfo,
+                                  Phone_No: e.target.value,
+                                })
+                              }
+                            />
+                          </label>
+                          <br />
+                          <label>
+                            Blood Group:
+                            <select
+                              value={newUserInfo.Blood_Group}
+                              onChange={(e) =>
+                                setNewUserInfo({
+                                  ...newUserInfo,
+                                  Blood_Group: e.target.value,
+                                })
+                              }
+                            >
+                              <option value="A+">A+</option>
+                              <option value="A-">A-</option>
+                              <option value="B+">B+</option>
+                              <option value="B-">B-</option>
+                              <option value="O+">O+</option>
+                              <option value="O-">O-</option>
+                              <option value="AB+">AB+</option>
+                              <option value="AB-">AB-</option>
+                              <option value="Unknown">Unknown</option>
+                            </select>
+                          </label>
+                          <br />
+                          <label>
+                            Last Donation Date
+                            <input
+                              type="date"
+                              value={newUserInfo.Last_Donation_Date}
+                              onChange={(e) =>
+                                setNewUserInfo({
+                                  ...newUserInfo,
+                                  Last_Donation_Date: e.target.value,
+                                })
+                              }
+                            />
+                          </label>
+                          <button onClick={handleChangeUserInfo}>
+                            Save Changes
+                          </button>
+                          <button
+                            onClick={() => {
+                              setIsEditing(false); // Stop editing mode
+                              setIsDeleteButtonDisabled(false); // Disable delete button
+                            }}
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  )}
+              </React.Fragment>
             ))}
-          </select>
-        </div>
-        {selectedUser && (
-          <div className="user-details-container">
-            <h2>User Information</h2>
-            <p>User ID: {selectedUser.User_ID}</p>
-            <p>First Name: {selectedUser.First_Name}</p>
-            <p>Middle Name: {selectedUser.Middle_Name || "N/A"}</p>
-            <p>Last Name: {selectedUser.Last_Name}</p>
-            <p>Username: {selectedUser.Username}</p>
-            <p>Password: {selectedUser.Password}</p>
-            <p>Email: {selectedUser.Email}</p>
-            <p>Phone Number: {selectedUser.Phone_No}</p>
-            <p>Blood Group: {selectedUser.Blood_Group}</p>
-            <p>Last Donation Date: {selectedUser.Last_Donation_Date}</p>
-
-            {isEditing ? (
-              // Render a form for editing the user info
-              <div>
-                <h2>Edit User Information</h2>
-                <label>
-                  First Name:
-                  <input
-                    type="text"
-                    value={newUserInfo.First_Name}
-                    onChange={(e) =>
-                      setNewUserInfo({
-                        ...newUserInfo,
-                        First_Name: e.target.value,
-                      })
-                    }
-                  />
-                </label>
-                <br />
-                <label>
-                  Middle Name:
-                  <input
-                    type="text"
-                    value={newUserInfo.Middle_Name}
-                    onChange={(e) =>
-                      setNewUserInfo({
-                        ...newUserInfo,
-                        Middle_Name: e.target.value,
-                      })
-                    }
-                  />
-                </label>
-                <br />
-                <label>
-                  Last Name:
-                  <input
-                    type="text"
-                    value={newUserInfo.Last_Name}
-                    onChange={(e) =>
-                      setNewUserInfo({
-                        ...newUserInfo,
-                        Last_Name: e.target.value,
-                      })
-                    }
-                  />
-                </label>
-                <br />
-                <label>
-                  Username:
-                  <input
-                    type="text"
-                    value={newUserInfo.Username}
-                    onChange={(e) =>
-                      setNewUserInfo({
-                        ...newUserInfo,
-                        Username: e.target.value,
-                      })
-                    }
-                  />
-                </label>
-                <br />
-                <label>
-                  Password:
-                  <input
-                    type="text"
-                    value={newUserInfo.Password}
-                    onChange={(e) =>
-                      setNewUserInfo({
-                        ...newUserInfo,
-                        Password: e.target.value,
-                      })
-                    }
-                  />
-                </label>
-                <br />
-                <label>
-                  Email:
-                  <input
-                    type="text"
-                    value={newUserInfo.Email}
-                    onChange={(e) =>
-                      setNewUserInfo({
-                        ...newUserInfo,
-                        Email: e.target.value,
-                      })
-                    }
-                  />
-                </label>
-                <br />
-                <label>
-                  Phone No:
-                  <input
-                    type="text"
-                    value={newUserInfo.Phone_No}
-                    onChange={(e) =>
-                      setNewUserInfo({
-                        ...newUserInfo,
-                        Phone_No: e.target.value,
-                      })
-                    }
-                  />
-                </label>
-                <br />
-                <label>
-                  Blood Group:
-                  <select
-                    value={newUserInfo.Blood_Group}
-                    onChange={(e) =>
-                      setNewUserInfo({
-                        ...newUserInfo,
-                        Blood_Group: e.target.value,
-                      })
-                    }
-                  >
-                    <option value="A+">A+</option>
-                    <option value="A-">A-</option>
-                    <option value="B+">B+</option>
-                    <option value="B-">B-</option>
-                    <option value="O+">O+</option>
-                    <option value="O-">O-</option>
-                    <option value="AB+">AB+</option>
-                    <option value="AB-">AB-</option>
-                    <option value="Unknown">Unknown</option>
-                  </select>
-                </label>
-                <br />
-                <label>
-                  Last Donation Date
-                  <input
-                    type="date"
-                    value={newUserInfo.Last_Donation_Date}
-                    onChange={(e) =>
-                      setNewUserInfo({
-                        ...newUserInfo,
-                        Last_Donation_Date: e.target.value,
-                      })
-                    }
-                  />
-                </label>
-                <br />
-                <button onClick={handleChangeUserInfo}>Save Changes</button>
-                <button onClick={handleDeleteUser}>Delete User</button>
-              </div>
-            ) : (
-              // Render a button to enable editing mode
-              <button onClick={() => setIsEditing(true)}>
-                Edit/Delete User Info
-              </button>
-            )}
-          </div>
-        )}
+          </tbody>
+        </table>
       </div>
     </div>
   );
