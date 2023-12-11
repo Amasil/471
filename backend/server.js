3dea278944fff2f63ed3aaf1fb22e3e76558e6cd
zainab1712 <zainabwaseem1703@gmail.com>
2023-12-10 22:23:16 -0700

    comments

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Establishing a connection to the MySQL database
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -22,7 +23,6 @@ connection.connect((err) => {
     console.error("Error connecting to the database: " + err.stack);
     return;
   }
-
   console.log("Connected to the database.");
 });
 
@@ -34,12 +34,13 @@ app.get("/user", (req, res) => {
       res.status(500).send("Error querying the database.");
       return;
     }
-
     res.send(results);
   });
 });
+
 // PUT route to update user information
 app.put("/user", (req, res) => {
+  // Extracting data from the request body
   const {
     User_ID,
     First_Name,
@@ -53,6 +54,7 @@ app.put("/user", (req, res) => {
     Last_Donation_Date,
   } = req.body;
 
+  // Validating required fields
   if (
     !User_ID ||
     !First_Name ||
@@ -66,6 +68,7 @@ app.put("/user", (req, res) => {
     return;
   }
 
+  // SQL query for updating user information
   const updateQuery = `
     UPDATE User
     SET
@@ -94,19 +97,20 @@ app.put("/user", (req, res) => {
     User_ID,
   ];
 
+  // Executing the update query
   connection.query(updateQuery, values, (err, results) => {
     if (err) {
       console.error("Error updating the database: " + err.stack);
       res.status(500).send("Error updating the database.");
       return;
     }
-
     res.send("User updated successfully.");
   });
 });
 
 // POST route to insert a new user
 app.post("/user", (req, res) => {
+  // Extracting data from the request body
   const {
     First_Name,
     Middle_Name,
@@ -118,7 +122,8 @@ app.post("/user", (req, res) => {
     Blood_Group,
     Last_Donation_Date,
   } = req.body;
-  console.log(req.body);
+
+  // Validating required fields
   if (
     !First_Name ||
     !Last_Name ||
@@ -131,6 +136,7 @@ app.post("/user", (req, res) => {
     return;
   }
 
+  // SQL query for inserting a new user
   const insertQuery = `
     INSERT INTO User (
       First_Name,
@@ -157,25 +163,28 @@ app.post("/user", (req, res) => {
     Last_Donation_Date,
   ];
 
+  // Executing the insert query
   connection.query(insertQuery, values, (err, results) => {
     if (err) {
       console.error("Error inserting into the database: " + err.stack);
       res.status(500).send("Error inserting into the database.");
       return;
     }
-
     res.send("User inserted successfully.");
   });
 });
 
+// DELETE route to delete a user
 app.delete("/user", (req, res) => {
   const userId = req.body.User_ID;
 
+  // Validating required fields
   if (!userId) {
     res.status(400).send("User ID must be provided in the request body.");
     return;
   }
 
+  // SQL query for deleting a user
   const deleteQuery = `
     DELETE FROM User
     WHERE User_ID = ?
@@ -188,6 +197,7 @@ app.delete("/user", (req, res) => {
       return;
     }
 
+    // Checking if the user was found and deleted
     if (results.affectedRows === 0) {
       res.status(404).send("User not found.");
     } else {
@@ -198,6 +208,7 @@ app.delete("/user", (req, res) => {
 
 // GET route to fetch initial blood quantities
 app.get("/bloodQuantities", (req, res) => {
+  // SQL query to retrieve blood quantities from the Inventory table
   connection.query(
     "SELECT Blood_Type, No_of_units FROM Inventory",
     (err, results) => {
@@ -211,6 +222,7 @@ app.get("/bloodQuantities", (req, res) => {
         return;
       }
 
+      // Formatting the results into a more readable format
       const quantities = {};
       results.forEach((row) => {
         quantities[row.Blood_Type] = row.No_of_units;
@@ -223,13 +235,16 @@ app.get("/bloodQuantities", (req, res) => {
 
 // POST route to insert data into the Inventory table
 app.post("/inventory", (req, res) => {
+  // Extracting data from the request body
   const { Hospital_ID, No_of_units, Blood_type } = req.body;
 
+  // Validating required fields
   if (!Hospital_ID || !No_of_units || !Blood_type) {
     res.status(400).send("All required fields must be provided.");
     return;
   }
 
+  // SQL query for inserting data into the Inventory table
   const insertQuery = `
     INSERT INTO Inventory (Hospital_ID, No_of_units, Blood_type)
     VALUES (?, ?, ?)
@@ -237,6 +252,7 @@ app.post("/inventory", (req, res) => {
 
   const values = [Hospital_ID, No_of_units, Blood_type];
 
+  // Executing the insert query
   connection.query(insertQuery, values, (err, results) => {
     if (err) {
       console.error("Error inserting into the Inventory table: " + err.stack);
@@ -250,13 +266,16 @@ app.post("/inventory", (req, res) => {
 
 // PUT route to update blood quantities in the Inventory table
 app.put("/inventory", (req, res) => {
+  // Extracting data from the request body
   const { Hospital_ID, No_of_units, Blood_type } = req.body;
 
+  // Validating required fields
   if (!Hospital_ID || !No_of_units || !Blood_type) {
     res.status(400).send("All required fields must be provided.");
     return;
   }
 
+  // SQL query for updating blood quantities in the Inventory table
   const updateQuery = `
     UPDATE Inventory
     SET No_of_units = No_of_units + ?
@@ -265,6 +284,7 @@ app.put("/inventory", (req, res) => {
 
   const values = [No_of_units, Hospital_ID, Blood_type];
 
+  // Executing the update query
   connection.query(updateQuery, values, (err, results) => {
     if (err) {
       console.error("Error updating blood quantity: " + err.stack);
@@ -276,6 +296,7 @@ app.put("/inventory", (req, res) => {
   });
 });
 
+// Starting the server on port 3000
 app.listen(3000, () => {
   console.log("Server listening on port 3000.");
 });
