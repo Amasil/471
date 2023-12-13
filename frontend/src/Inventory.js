bac8a57157af844dbe8c325cd9747d06f1c952f5
zainab1712 <zainabwaseem1703@gmail.com>
2023-12-12 17:36:51 -0700

    .

--- frontend/src/Inventory.js
+++ frontend/src/Inventory.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./inventory.css";
+import "./Inventory.css";
 
 const Inventory = () => {
   const [editableQuantities, setEditableQuantities] = useState({
