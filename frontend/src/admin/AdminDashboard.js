005bc7f34e186dc9adb05ade9cebf23ca939d34a
yaseminkhan <ykhanmoradi@gmail.com>
2023-12-18 21:56:58 -0700

    fixed tupe for admin adding recipient

--- frontend/src/admin/AdminDashboard.js
+++ frontend/src/admin/AdminDashboard.js
@@ -428,7 +428,7 @@ const Users = () => {
                                 <option value="Donor">Donor</option>
                                 <option value="Receipient">Receipient</option>
                                 <option value="Doctor">Doctor</option>
-                                <option value="Recipient">Doctor</option>
+                                <option value="Recipient">Recipient</option>
                               </select>
                             </label>
                             <br />
