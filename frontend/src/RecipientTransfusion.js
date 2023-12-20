83f7a4d31ba850982acdbf7783b97c1649177437
yaseminkhan <ykhanmoradi@gmail.com>
2023-12-20 08:18:01 -0700

    error fix

--- frontend/src/RecipientTransfusion.js
+++ frontend/src/RecipientTransfusion.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 const RecipientAppointments = () => {
     const [appointments, setAppointments] = useState([]);
     const [recipientId, setRecipientId] = useState(""); 
